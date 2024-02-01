d0c0667cdfd5e250d0360d89a43511ae731d9ce5
Snehil <sdupworkuser@gmail.com>
2024-02-01 10:53:16 +0530

    uploaded tm-time-measurement.module.ts file

---
+++ src/app/time-measurement/tm-time-measurement.module.ts
@@ -0,0 +1,34 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CtCommonToolsModule} from '../common-tools/ct-common-tools.module';
+import {TmTimeMeasurementComponent} from './tm-time-measurement.component';
+import {TmTimeTableComponent} from './tm-time-table/tm-time-table.component';
+import {TmTimerComponent} from './tm-timer/tm-timer.component';
+import {TmSettingsComponent} from './tm-settings/tm-settings.component';
+import {TmTimeMeasurementService} from './tm-time-measurement.service';
+import {TmTimeTableTimePipe} from './tm-time-table/tm-time-table-time.pipe';
+import {TmTimeTableDurationPipe} from './tm-time-table/tm-time-table-duration.pipe';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CtCommonToolsModule
+  ],
+  declarations: [
+    TmTimeMeasurementComponent,
+    TmTimerComponent,
+    TmTimeTableComponent,
+    TmSettingsComponent,
+    TmTimeTableTimePipe,
+    TmTimeTableDurationPipe
+  ],
+  providers: [TmTimeMeasurementService],
+  exports: [
+    TmTimeMeasurementComponent
+  ]
+})
+export class TmTimeMeasurementModule {
+}
